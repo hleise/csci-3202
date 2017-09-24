0f964bdbe64d66a8bb1a4cb858d1061b88ca8e1d
Hunter Leise <hleise27@gmail.com>
2017-09-24 00:48:33 -0600

    Working DFS!

--- 1 - Search/lab1.py
+++ 1 - Search/lab1.py
@@ -1,4 +1,8 @@
-from copy import *
+class ProblemSpace:
+    def __init__(self, _startingState, _capacity):
+        self.start = _startingState
+        self.capacity = _capacity
+capacity = [40, 40, 5, 4]
 
 def milkDFS(start):
     stack = [(start, [start])]
@@ -6,18 +10,18 @@ def milkDFS(start):
 
     while stack:
         (state, path) = stack.pop()
-        print(getState(state))
-        visited.append(getState(state))
+        visited.append(state)
         children = getChildren(state)
         for child in children:
-            child2 = deepcopy(child)
+            child2 = child.copy()
             child2[0], child2[1] = child2[1], child2[0]
 
             if (child not in visited) and (child2 not in visited):
-                if child[2].contents == 2 and child[3].contents == 2:
+                if child[2] == 2 and child[3] == 2:
                     return path + [child]
                 else:
-                    stack.append((child, path + [child]))
+                    stack.append((child, path + [child])) #DFS
+                    # stack.insert(0, (child, path + [child])) # BFS
     return []
 
 def getChildren(state):
@@ -25,46 +29,26 @@ def getChildren(state):
 
     for cupFrom in range(0, len(state)):
         for cupTo in range(0, len(state)):
-            if (not state[cupFrom].isEmpty()) and (not state[cupTo].isFull()) and (cupFrom != cupTo):
-                childState = deepcopy(state)
-                if childState[cupFrom].contents > childState[cupTo].getOpenSpace():
-                    childState[cupFrom].contents -= childState[cupTo].getOpenSpace()
-                    childState[cupTo].contents = childState[cupTo].capacity
+            if (state[cupFrom] > 0) and (not isFull(state, cupTo)) and (cupFrom != cupTo):
+                childState = state.copy()
+                if childState[cupFrom] > getOpenSpace(childState, cupTo):
+                    childState[cupFrom] -= getOpenSpace(childState, cupTo)
+                    childState[cupTo] = capacity[cupTo]
                 else:
-                    childState[cupTo].contents += childState[cupFrom].contents
-                    childState[cupFrom].contents = 0
+                    childState[cupTo] += childState[cupFrom]
+                    childState[cupFrom] = 0
                 children.append(childState)
 
     return children
 
-def getState(state):
-    stateArray = []
-    for cup in state:
-        stateArray.append(cup.contents)
-    return stateArray
+def isFull(state, index):
+    return True if state[index] == capacity[index] else False
 
-class Cup:
-    def __init__(self, capacity, contents):
-        self.capacity = capacity
-        self.contents = contents
-
-    def __eq__(self, other):
-        if self.capacity.value == other.capacity.value:
-            return True
-        else:
-            False
-
-    def getOpenSpace(self):
-        return self.capacity - self.contents
-
-    def isFull(self):
-        return True if self.capacity == self.contents else False
-
-    def isEmpty(self):
-        return True if self.contents == 0 else False
+def getOpenSpace(state, index):
+    return capacity[index] - state[index]
 
 def main():
-    startingState = [Cup(40, 40), Cup(40, 40), Cup(5, 0), Cup(4, 0)]
-    milkDFS(startingState)
+    startingState = [40, 40, 0, 0]
+    print(milkDFS(startingState))
 
 main()
