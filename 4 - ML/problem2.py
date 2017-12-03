06f98ad2a78660749d14abe1c717d9ed2d5a45c7
Hunter Leise <hleise27@gmail.com>
2017-12-02 17:57:37 -0700

    Make 4.2 more efficient by not checking houses of type 0.

--- 4 - ML/problem2.py
+++ 4 - ML/problem2.py
@@ -12,10 +12,13 @@ class City:
         self.vacant = [i for i, x in enumerate(self.neighbors) if x == 0]
 
     def iterate(self, iterations):
-        for _ in range(iterations):
+        for iteration in range(iterations):
+            if iteration % 20 == 0:
+                print(self.neighbors)
+
             dissatisfied = []
-            for i in self.neighbors:
-                if not self.is_satisfied(self.neighbors[i]) and self.neighbors[i] != 0:
+            for i in range(len(self.neighbors)):
+                if self.neighbors[i] != 0 and not self.is_satisfied(i):
                     dissatisfied.append(i)
             if len(dissatisfied) == 0:
                 print("all satisfied")
@@ -29,16 +32,15 @@ class City:
 
     def is_satisfied(self, house):
         same_neighbors = 0
-        for i in range(1, 2):
-            if self.neighbors[house + i] == self.neighbors[house]:
+        for i in range(1, 3):
+            if self.neighbors[(house + i) % len(self.neighbors)] == self.neighbors[house]:
                 same_neighbors += 1
-            if self.neighbors[house - i] == self.neighbors[house]:
+            if self.neighbors[(house - i) % len(self.neighbors)] == self.neighbors[house]:
                 same_neighbors += 1
         return same_neighbors >= 2
 
 
 if __name__ == '__main__':
     city = City()
-    print(city.neighbors)
     city.iterate(400)
     print(city.neighbors)
