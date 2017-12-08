2cdefd0eeaa22bd1ecb234d98ef1e6efdf71293c
Hunter Leise <hleise27@gmail.com>
2017-12-08 00:08:13 -0700

    Add in depth comments.

--- 4 - ML/problem1.py
+++ 4 - ML/problem1.py
@@ -8,15 +8,35 @@ import math
 
 
 class Perceptron:
+    """
+    Represents a perceptron and all of the methods necessary to train it.
+
+    Attributes:
+        num_inputs (int): number of inputs to the perceptron
+        correct (function([list of inputs]) --> bool): function that returns
+            the correct output given a list of inputs. Used for training.
+        bias (float): initial perceptron bias
+        weights (list of float): list of input weights
+    """
     def __init__(self, num_inputs, correct_func, bias):
         self.num_inputs = num_inputs
         self.correct = correct_func
         self.bias = bias
         self.weights = self.initialize_weights(self, num_inputs, -1, 1)
-        self.errors = 0
 
     @staticmethod
     def initialize_weights(self, num_weights, range_min, range_max):
+        """
+        Randomize initial weights before training.
+
+        Args:
+            num_weights (int): number of weights to create
+            range_min (int): minimum weight for randomization
+            range_max (int): maximum weight for randomization
+
+        Returns:
+            list of floats: list of weights for each input
+        """
         weights = []
         for _ in range(num_weights):
             weights.append(random.uniform(range_min, range_max))
@@ -24,47 +44,92 @@ class Perceptron:
 
     @staticmethod
     def prediction(self, output):
+        """
+        Takes a sigmoid output value and returns its corresponding
+        boolean value.
+
+        Args:
+            output (float): sigmoid activation value
+        Returns:
+            bool: True if output >= 0.5
+                False otherwise
+        """
         return True if output >= 0.5 else False
 
     def train(self, iterations, learning_rate):
+        """
+        Train the perceptron for the given number of iterations.
+
+        Args:
+            iterations (int): number of training iterations
+            learning_rate (float): learning rate
+        Returns:
+            None
+        """
         for i in range(iterations):
             if i % 250 == 0:  # print weights every 250 iterations
                 print(self.weights)
             inputs = [random.randint(0, 1) for _ in range(self.num_inputs)]
             output = self.sig_output(inputs)
             if self.prediction(self, output) != get_correct(inputs):
-                self.errors += 1
                 derivative = output * (1.0 - output)
                 error = self.correct(inputs) - output
                 self.update(learning_rate, error, derivative, inputs)
 
     def sig_output(self, inputs):
+        """
+        Calculate sigmoid activation function output
+
+        Args:
+            inputs (list of int): list of binary inputs
+        Returns:
+            float: sigmoid activation value
+        """
         weighted_sum = self.bias
         for i in range(len(inputs)):
             weighted_sum += inputs[i] * self.weights[i]
         return 1.0 / (1.0 + math.exp(-weighted_sum))
 
     def update(self, learning_rate, error, derivative, inputs):
+        """
+        Update the weights using the perceptron update function
+
+        Args:
+            learning_rate (float): learning rate
+            error (float): calculated error
+            derivative (float): sigmoid derivative
+            inputs (list of int): list of perceptron binary inputs
+        Returns:
+            None
+        """
         for i in range(len(self.weights)):
-            self.weights[i] = self.weights[i] + (learning_rate * error * derivative * inputs[i])
+            self.weights[i] = self.weights[i] + \
+                              (learning_rate * error * derivative * inputs[i])
 
 
 def get_correct(inputs):
     """
-    Returns the correct output (and function of inputs a and c)
-    given a list of inputs.
+    Return the 'and' function of inputs 1 and 3.
 
     Args:
-        inputs (integer list): list of integer inputs 0 or 1
-
+        inputs (list of int): list of binary inputs
     Returns:
-        1 for True, 0 for False
-
+        int: 1 for True (inputs 1 and 3 are both 1)
+            0 for False (either inputs 1 or 3 are 0)
     """
     return int(inputs[0] and inputs[2])
 
 
 def all_correct(perceptron):
+    """
+    Check if all possible 3 input combinations return the expected values.
+
+    Args:
+        perceptron (Perceptron): perceptron class instance
+    Returns:
+        bool: True if all input possibilities return the expected values,
+            False otherwise.
+    """
     for i in range(2):
         for j in range(2):
             for k in range(2):
@@ -79,5 +144,3 @@ def all_correct(perceptron):
 if __name__ == '__main__':
     perceptron = Perceptron(3, get_correct, -1)
     perceptron.train(8000, 1.0)
-    print(all_correct(perceptron))
-    print(perceptron.errors)
--- 4 - ML/problem2.py
+++ 4 - ML/problem2.py
@@ -6,24 +6,41 @@ import random
 
 
 class City:
+    """
+    Represents a ring city and the necessary methods for how it operates.
+
+    Attributes:
+        neighbors (list of int): list of neighbor types
+        vacant (list of int): indexes of vacant homes in the neighbors list.
+    """
     def __init__(self):
         self.neighbors = [0] * 6 + [1] * 27 + [2] * 27
         random.shuffle(self.neighbors)
         self.vacant = [i for i, x in enumerate(self.neighbors) if x == 0]
 
     def iterate(self, iterations):
+        """
+        Iterate until all neighbors are satisfied or until the maximum
+        number of iterations is reached.
+
+        Args:
+            iterations (int): Maximum number of times the cycle will be run
+
+        Returns:
+            None
+        """
         for iteration in range(iterations):
-            if iteration % 20 == 0:
+            if iteration % 20 == 0:  # print every 20 iterations
                 print(self.neighbors)
 
             dissatisfied = []
             for i in range(len(self.neighbors)):
                 if self.neighbors[i] != 0 and not self.is_satisfied(i):
                     dissatisfied.append(i)
-            if len(dissatisfied) == 0:
+            if len(dissatisfied) == 0:  # Yay! Everyone is satisfied!
                 print("all satisfied")
                 break
-            else:
+            else:  # Move unsatisfied people to vacant house
                 rand_dis = random.choice(dissatisfied)
                 self.neighbors[self.vacant[0]] = self.neighbors[rand_dis]
                 self.neighbors[rand_dis] = 0
@@ -31,11 +48,24 @@ class City:
                 self.vacant.append(rand_dis)
 
     def is_satisfied(self, house):
+        """
+        Returns whether a given house is satisfied. In other words,
+        whether it has at least two neighbors of its own type within
+        two houses on either side.
+
+        Args:
+            house (int): index for the queried house
+
+        Returns:
+             bool: True if satisfied, False if not satisfied
+        """
         same_neighbors = 0
         for i in range(1, 3):
-            if self.neighbors[(house + i) % len(self.neighbors)] == self.neighbors[house]:
+            if self.neighbors[(house + i) % len(self.neighbors)] \
+                    == self.neighbors[house]:
                 same_neighbors += 1
-            if self.neighbors[(house - i) % len(self.neighbors)] == self.neighbors[house]:
+            if self.neighbors[(house - i) % len(self.neighbors)] \
+                    == self.neighbors[house]:
                 same_neighbors += 1
         return same_neighbors >= 2
 
@@ -43,4 +73,6 @@ class City:
 if __name__ == '__main__':
     city = City()
     city.iterate(400)
-    print(city.neighbors)
+    print(city.neighbors)  # Print final neighborhood
+
+f
